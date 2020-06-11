d863faf6aab10457858d4bf751a3271bcf740a92
alexisg1342 <giovannyy1342@gmail.com>
2020-06-11 16:47:43 -0500

    Initial commit

---
+++ vistas/modulos/404.php
@@ -0,0 +1,39 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+           
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Error 404</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="error-page">
+
+        <h2 class="headline text-warning"> <font color = 'red'>404</font></h2>
+
+        <div class="error-content">
+          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Lo siento, pagina no encontrada.</h3>
+
+          <p>
+            puedes volver al sistema dando clic aqui <a href="../../index.php">retornar al inicio</a>
+          </p>
+
+          
+        </div>
+        <!-- /.error-content -->
+      </div>
+      <!-- /.error-page -->
+    </section>
+    <!-- /.content -->
+  </div>
---
+++ vistas/modulos/menu.php
@@ -0,0 +1,91 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="vistas/img/logo-cronos.jpg" alt="Cronos Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">Cronos</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="vistas/img/usuario-logo.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Nombre del usuario</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-header">MENU</li>
+            <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+            <i class="fas fa-user"></i>
+              <p>
+                Usarios
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="usuario" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Ver Usuario</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="grupo" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Ver Grupos</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+            <i class="fas fa-project-diagram"></i>
+              <p>
+              Proyectos
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="proyecto" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Proyectos</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="metodologia" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Metodologia</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="fases" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Fases</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="modulos" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Modulos</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        
+        </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
